6e719209acbf4fd981e31e9af6e825d3ddcd8016
Mevin5611 <mevinmevi88@gmail.com>
2024-03-17 17:43:48 +0530

    payment functionality and api create order redis updated

--- controllers/order.controller.ts
+++ controllers/order.controller.ts
@@ -1,99 +1,161 @@
-import { NextFunction,Request,Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { CatchAsyncError } from "../middleware/catchAsyncError";
 import ErrorHandler from "../utils/ErrorHandler";
 import orderModel, { IOrder } from "../models/order.model";
 import CourseModel from "../models/course.model";
 import userModel from "../models/user.model";
 import path from "path";
-import ejs from 'ejs'
+import ejs from "ejs";
 import sendMail from "../utils/sendMail";
 import notificationModel from "../models/notification.model";
 import { getAllOrderServices, newOrder } from "../services/order.service";
+import { redis } from "../utils/redis";
+require("dotenv").config()
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 // create order
 
- export const createOrder = CatchAsyncError(async(req:Request,res:Response,next:NextFunction)=>{
+export const createOrder = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const {courseId,payment_info} = req.body as IOrder;
+      const { courseId, payment_info } = req.body as IOrder;
 
-        const user = await userModel.findById(req.user?._id)
+      const user = await userModel.findById(req.user?._id);
 
-        const courseExistInUser = user?.courses.some((course:any)=> course._id.toString() === courseId)
+      const courseExistInUser = user?.courses.some(
+        (course: any) => course._id.toString() === courseId
+      );
+        if(payment_info){
 
-        if(courseExistInUser){
-            return next(new ErrorHandler("You have alredy purchased this course",400))
-        }
-        const course = await CourseModel.findById(courseId)
-
-        if(!course){
-            return next(new ErrorHandler("course not found",400))
-        }
-
-        const data:any ={
-            courseId:course._id,
-            userId:user?._id,
-            payment_info,
-        }
-
-        
-
-        const mailData={
-            order:{
-                _id:course._id.toString().slice(0,6),
-                name:course.name,
-                benefits:course.benefits,
-                price:course.price,
-                date:new Date().toLocaleDateString('en-US',{year:"numeric",month:"long",day:'numeric'})
+            if("id" in payment_info){
+              const paymentIntentId = payment_info.id;
+              const paymentIntent = await stripe.paymentIntents.retrieve(
+                paymentIntentId
+              )
+              if(paymentIntent.status !== "succeeded"){
+               return next(new ErrorHandler("payment not authorized",400))
+              }
             }
+
+            
         }
 
-        const html = await ejs.renderFile(path.join(__dirname,"../mails/order-confirmation.ejs"),{order:mailData})
+      if (courseExistInUser) {
+        return next(
+          new ErrorHandler("You have alredy purchased this course", 400)
+        );
+      }
+      const course = await CourseModel.findById(courseId);
 
-        try {
-            if(user){
-                await sendMail({
-                    email: user?.email,
-                    subject: "Order Confirmation",
-                    template: "order-confirmation.ejs",
-                    data:mailData,
-                  });
-            }
-          } catch (error: any) {
-            return next(new ErrorHandler(error.message, 500));
-          }
+      if (!course) {
+        return next(new ErrorHandler("course not found", 400));
+      }
 
-          user?.courses.push(course?._id)
+      const data: any = {
+        courseId: course._id,
+        userId: user?._id,
+        payment_info,
+      };
 
-          await user?.save();
+      const mailData = {
+        order: {
+          _id: course._id.toString().slice(0, 6),
+          name: course.name,
+          benefits: course.benefits,
+          price: course.price,
+          date: new Date().toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          }),
+        },
+      };
 
-         await notificationModel.create({
-            user:user?._id,
-            title: "New Order",
-            message: `You Have a new order from ${course?.name}`,
+      const html = await ejs.renderFile(
+        path.join(__dirname, "../mails/order-confirmation.ejs"),
+        { order: mailData }
+      );
+
+      try {
+        if (user) {
+          await sendMail({
+            email: user?.email,
+            subject: "Order Confirmation",
+            template: "order-confirmation.ejs",
+            data: mailData,
           });
+        }
+      } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
+      }
 
-          
-          course.purchased = (course.purchased || 0) + 1 
+      user?.courses.push(course?._id);
 
-       
-          await course.save();
+      await redis.set(req.user?._id,JSON.stringify(user))
 
-          newOrder(data,res,next)
+      await user?.save();
 
+      await notificationModel.create({
+        user: user?._id,
+        title: "New Order",
+        message: `You Have a new order from ${course?.name}`,
+      });
 
+      course.purchased = (course.purchased || 0) + 1;
 
-    } catch (error:any) {
-        return next(new ErrorHandler(error.message,500))
+      await course.save();
+
+      newOrder(data, res, next);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
     }
- })
+  }
+);
 
- // get all orders only for admins
- 
- export const getAllOrders = CatchAsyncError(async(req:Request,res:Response,next:NextFunction)=>{
+// get all orders only for admins
+
+export const getAllOrders = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        getAllOrderServices(res)
-    } catch (error:any) {
-        return next(new ErrorHandler(error.message,500))
+      getAllOrderServices(res);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
     }
- })
+  }
+);
+
+// send stripe publishible key
+
+export const sendStripePublishibleKey = CatchAsyncError(async(req: Request, res: Response)=>{
+res.status(200).json({
+    publishablekey:process.env.STRIPE_PUBLISHABLE_KEY
+})
+})
+
+// new payment
+
+export const newPayment = CatchAsyncError(
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const myPayment = await stripe.paymentIntents.create({
+            amount:req.body.amount,
+            currency:"INR",
+            metadata:{
+                company:"Cybrogix",
+            },
+            automatic_payment_methods:{
+                enabled:true,
+            }
+        })
+
+        res.status(200).json({
+            success:true,
+            client_secret:myPayment.client_secret
+        })
+       
+      } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
+      }
+    }
+  );
