ffdb36256dddcea8036a0de47cc2296581cdd3fc
Mevin5611 <mevinmevi88@gmail.com>
2024-03-17 17:44:55 +0530

    payment routes added

--- routes/order.route.ts
+++ routes/order.route.ts
@@ -1,9 +1,21 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
-import { createOrder, getAllOrders } from "../controllers/order.controller";
+import {
+  createOrder,
+  getAllOrders,
+  newPayment,
+  sendStripePublishibleKey,
+} from "../controllers/order.controller";
 const orderRouter = express.Router();
 
 orderRouter.post("/create-order", isAuthenticated, createOrder);
-orderRouter.get("/get-all-orders", isAuthenticated,authorizeRoles("admin"),getAllOrders);
+orderRouter.get(
+  "/get-all-orders",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  getAllOrders
+);
+orderRouter.get("/payment/stripepublishablekey", sendStripePublishibleKey);
+orderRouter.post("/payment", isAuthenticated, newPayment);
 
 export default orderRouter;
