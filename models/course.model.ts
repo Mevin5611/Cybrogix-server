7828c1d7d42ab61a9f4a4e501ee705c5237fe70d
Mevin5611 <mevinmevi88@gmail.com>
2024-03-20 21:59:08 +0530

    timestamps added in question,questionreplay,comment schemas

--- models/course.model.ts
+++ models/course.model.ts
@@ -60,18 +60,23 @@ const reviewSchema = new Schema<IReview>({
     },
     comment:String,
     commentReplies:[Object]
-})
+},{timestamps:true})
 
 const linkSchema = new Schema<ILink>({
     title:String,
     url:String,
 })
+const commentReplySchema = new Schema({
+    user: Object,
+    answer: String
+}, { timestamps: true });
 
 const commentSchema = new Schema<IComment>({
     user:Object,
     question:String,
-    questionReplies:[Object],
-})
+    questionReplies:[commentReplySchema],
+
+},{timestamps:true})
 
 const courseDataSchema = new Schema<ICourseData>({
     videoUrl:String,
