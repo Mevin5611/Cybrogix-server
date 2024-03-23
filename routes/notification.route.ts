b2e94896dd3c2f1484fbdc3ae1b2219f25a4b02d
Mevin5611 <mevinmevi88@gmail.com>
2024-03-23 23:48:15 +0530

    notification route removed update access token and isauthenticated and autharize role

--- routes/notification.route.ts
+++ routes/notification.route.ts
@@ -1,19 +1,18 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 import { getNotification, updateNotification } from "../controllers/notification.controller.moder";
+import { updateAccessToken } from "../controllers/user.controller";
 
 const notificationRoute = express.Router();
 
 notificationRoute.get(
   "/get-all-notifications",
-  isAuthenticated,
-  authorizeRoles("admin"),
+  
   getNotification
 );
 notificationRoute.put(
   "/update-notification/:id",
-  isAuthenticated,
-  authorizeRoles("admin"),
+  
   updateNotification
 );
 
