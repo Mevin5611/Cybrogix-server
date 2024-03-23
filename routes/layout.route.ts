dbb19d988a6d70eb2d7d35174c505a9a91018044
Mevin5611 <mevinmevi88@gmail.com>
2024-03-23 23:46:54 +0530

    layout route also added update access token

--- routes/layout.route.ts
+++ routes/layout.route.ts
@@ -1,17 +1,20 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 import { createLayout, editLayout, getLayout } from "../controllers/layout.controller";
+import { updateAccessToken } from "../controllers/user.controller";
 
 const layoutRoute = express.Router();
 
 layoutRoute.post(
   "/create-layout",
+  updateAccessToken,
   isAuthenticated,
   authorizeRoles("admin"),
   createLayout
 );
 layoutRoute.put(
   "/edit-layout",
+  updateAccessToken,
   isAuthenticated,
   authorizeRoles("admin"),
   editLayout
