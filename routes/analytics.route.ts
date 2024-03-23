d2843c6a86b96d23b66bfa756c88562b8811afb8
Mevin5611 <mevinmevi88@gmail.com>
2024-03-23 23:46:16 +0530

    all analytics route added updateaccess token

--- routes/analytics.route.ts
+++ routes/analytics.route.ts
@@ -1,23 +1,27 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 import { getCourseAnlytics, getOrderAnlytics, getUserAnlytics } from "../controllers/analytics.controller";
+import { updateAccessToken } from "../controllers/user.controller";
 
 const analyticsRoute = express.Router();
 
 analyticsRoute.get(
   "/get-user-analytics",
+  updateAccessToken,
   isAuthenticated,
   authorizeRoles("admin"),
   getUserAnlytics
 );
 analyticsRoute.get(
   "/get-course-analytics",
+  updateAccessToken,
   isAuthenticated,
   authorizeRoles("admin"),
   getCourseAnlytics
 );
 analyticsRoute.get(
   "/get-order-analytics",
+  updateAccessToken,
   isAuthenticated,
   authorizeRoles("admin"),
   getOrderAnlytics
