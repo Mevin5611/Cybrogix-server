7f962d591822857483927f0a6e8d464114fd02c7
Mevin5611 <mevinmevi88@gmail.com>
2024-04-03 13:48:19 +0530

    deploy vercel

--- app.ts
+++ app.ts
@@ -20,6 +20,12 @@ app.use(express.json({ limit: "50mb" }));
 
 app.use(cookieParser());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 //cors
 app.use(
   cors({
