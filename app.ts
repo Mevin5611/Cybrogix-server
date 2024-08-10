e0e7bcb5ac7e31eb6f71dc828be091aa812eba4b
Mevin5611 <mevinmevi88@gmail.com>
2024-08-10 14:02:51 +0530

    payment error checking

--- app.ts
+++ app.ts
@@ -24,7 +24,7 @@ app.use(cookieParser());
 //cors
 app.use(
   cors({
-    origin:["https://cybrogix-client.vercel.app"],
+    origin:["https://cybrogix-client.vercel.app/"],
     credentials:true,
   })
 );
--- routes/order.route.ts
+++ routes/order.route.ts
@@ -6,16 +6,18 @@ import {
   newPayment,
   sendStripePublishibleKey,
 } from "../controllers/order.controller";
+import { updateAccessToken } from "../controllers/user.controller";
 const orderRouter = express.Router();
 
-orderRouter.post("/create-order", isAuthenticated, createOrder);
+orderRouter.post("/create-order",updateAccessToken, isAuthenticated, createOrder);
 orderRouter.get(
   "/get-all-orders",
+  updateAccessToken,
   isAuthenticated,
   authorizeRoles("admin"),
   getAllOrders
 );
 orderRouter.get("/payment/stripepublishablekey",sendStripePublishibleKey);
-orderRouter.post("/payment", isAuthenticated, newPayment);
+orderRouter.post("/payment",updateAccessToken, isAuthenticated, newPayment);
 
 export default orderRouter;
